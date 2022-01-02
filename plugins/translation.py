264245b5cd77ebf52a8dfe1dd1836fcbed92e6e3
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-02 09:48:58 +0530

    Update translation.py

--- plugins/translation.py
+++ plugins/translation.py
@@ -1,1 +1,52 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+class Script(object):
+
+
+
+    START_TEXT = """
+Hey {} 
+
+I am Instant Forward Tag Remover Bot
+
+I can Instantly delete Forward Tag.
+
+Add me to a channel with all Permissions and Forgot About Forward Tags on Your Post.
+"""
+    HELP_TEXT = """
+Recommended
+➠ Just Make Me admin in that Channel Where you want to remove tag and see magic
+"""
+    ABOUT_TEXT = """
+ **🤖 Bot :** Instant-Tag-Remover\n
+ **👲 Developer :** [Tellybots_4u](https://telegram.me/tellybots_4u)\n
+ **👥 Channel :** [Tellybots_4u](https://telegram.me/tellybots_4u)\n
+ **❄️ Credits :** Everyone in this journey\n
+ **🍴 Source :** [Click here](https://t.me/tellybots_digital)\n
+ **📝 Language :** [Python3](https://python.org)\n
+ **📚 Library :** [Pyrogram v1.2.20](https://pyrogram.org)\n
+ **🌟 Server :** [Heroku](https://heroku.com)\n
+"""
+    START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🤖 Update Channel', url='https://telegram.me/tellybots_4u'),
+        InlineKeyboardButton('💬 Support Group', url='https://telegram.me/tellybots_support')
+        ],[
+        InlineKeyboardButton('❔ Help', callback_data='help'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
+    HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏡 Home', callback_data='home'),
+        InlineKeyboardButton('👲 About', callback_data='about'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
+    ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏡 Home', callback_data='home'),
+        InlineKeyboardButton('❔ Help', callback_data='help'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
