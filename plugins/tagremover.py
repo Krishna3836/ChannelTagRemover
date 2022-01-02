b77d6ec60095af46da7c03a75cb126cca7b2f7e7
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-02 09:26:43 +0530

    Create tagremover.py

---
+++ plugins/tagremover.py
@@ -0,0 +1,17 @@
+
+
+
+
+       
+       
+   
+  
+@NaysaBots.on_message(filters.forwarded & filters.channel)
+async def channeltag(bot, message):
+   try:
+       chat_id = message.chat.id
+       forward_msg = await message.copy(chat_id)
+       await message.delete()
+   except:
+       await message.reply_text("Check My Admin Permissions and Try again")
+    
