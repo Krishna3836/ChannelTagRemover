271d03a2b5a99e63be8c5deb80eff5c076322d74
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-02 09:27:35 +0530

    Update tagremover.py

--- plugins/tagremover.py
+++ plugins/tagremover.py
@@ -7,7 +7,7 @@
    
   
 @NaysaBots.on_message(filters.forwarded & filters.channel)
-async def channeltag(bot, message):
+async def nstag(bot, message):
    try:
        chat_id = message.chat.id
        forward_msg = await message.copy(chat_id)
