0dc14d4d2e177bbde37da648d2bb033016dc17f7
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-02 09:50:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,1 +1,16 @@
+import os
+
+from plugins.translation import Translation
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@Client.on_message(filters.command(["start"]) & filters.private)
+async def start(bot, update):
+    
+    await update.reply_text(
+        text=Translation.START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=Translation.START_BUTTONS
+    )
 
